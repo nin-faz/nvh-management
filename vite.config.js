ca1ea92f00174cec172ebbbf91019d9bbe2fc903
nin-faz <fazernino@gmail.com>
2026-07-24 18:06:37 +0200

    ⚡️ performance : rate between 90 and 100

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default defineConfig({
+  plugins: [
+    ViteImageOptimizer({
+      jpg: { quality: 82 },
+      jpeg: { quality: 82 },
+      png: { quality: 82 },
+      webp: { lossless: false, quality: 82 },
+      includePublic: true,
+    }),
+  ],
+});
